320486962b4af30907e7e7cf8fcb3532172853ce
visionsl <rivesl@gmail.com>
2018-05-11 18:27:36 +0800

    增加读CPU温度值

--- src/com/main/Health.java
+++ src/com/main/Health.java
@@ -1,12 +1,10 @@
 package com.main;
 
+import com.common.Common;
 import com.common.FileUtil;
 import redis.clients.jedis.Jedis;
 
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import java.io.*;
 import java.util.Iterator;
 import java.util.Map;
 
@@ -37,7 +35,7 @@ public class Health {
     //Redis
     static Jedis _jedis;
     final static String _redis_Health_df_key = "disk_office2005";
-    private static int _info_df_maxv = 20;  //磁盘占用超过x%后提交上传
+    private static double _info_df_maxv = 20.0;  //磁盘占用超过x%后提交上传
     private String _info_df_fp;             //df信息文件路径, 在第1个参数中指定 args[0]
     private static String _cpu_serial;             //CPU序列号
 
@@ -76,25 +74,50 @@ public class Health {
 
     }
 
+    private static String getCpuTemprature(){
+        String fn = "/sys/class/thermal/thermal_zone0/temp";
+        String fc = FileUtil.getFileContent(fn);
+        double t = Double.parseDouble(fc)/1000;
+        return String.valueOf(t);
+    }
+
+    /**
+     * 读取CPU序列号
+     *      树莓派:    读取/proc/cpuinfo, 有一行"Serial          : 000000009755fccd"含有CPU序列号
+     *      Centos:    执行指令可得到序列号: dmidecode -t 4 | grep ID |sort -u |awk -F': ' '{print $2}' | sed '1d' | sed 's/ //g' > /???/link/cpuinfo
+     *                 在Centos中使用前, 需要用上面的命令把序列号生成到指定的文件中:Java程序所在的目录 /???/link/cpuinfo
+     * **/
     private static String getCpuserial(){
-        /**读取CPU序列号**/
         String fn = "/proc/cpuinfo";
         String fc = FileUtil.getFileContent(fn);
         if(fc==null){
             System.out.println("can't found :"+fn);
             return null;
         }
-        if(!fc.trim().equals("")){
+        if(!fc.trim().equals("") && fc.indexOf("Serial")!=-1){
             fc = fc.substring(fc.indexOf("Serial"));
             fc = fc.substring(fc.indexOf(":")+1);
-            if(fc!=null){
+            if(fc!=null && !fc.trim().equals("")){
+                //树莓派可用此方法
                 System.out.println(fc.trim());
                 return fc.trim();
             }
         }else{
             //读取CPU序列号失败, 直接退出
-            System.out.println("读取CPU序列号失败");
+            System.out.println("读取(树莓派)CPU序列号失败");
         }
+
+        //尝试用dmidecode读取CPU序列号
+        //Common.runCommand("dmidecode -t 4 | grep ID |sort -u |awk -F': ' '{print $2}' | sed '1d' | sed 's/ //g' > /data/link/cpuinfo", 1);
+        //System.out.println("尝试用dmidecode读取CPU序列号");
+        try{
+            File directory = new File("");
+            String cpufile = directory.getCanonicalPath()+"/cpuinfo";
+            fc = FileUtil.getFileContent(cpufile);
+            System.out.println("dmidecode:"+fc.trim());
+            return fc.trim();
+        }catch(Exception e){e.printStackTrace();}
+
         return null;
     }
 
@@ -105,12 +128,14 @@ public class Health {
     private static void scan_info_df(Health health) {
         try {
             String rediskey = _redis_Health_df_key+"_"+_cpu_serial;
+            Map<String, String> hmapget = _jedis.hgetAll(rediskey);
+            int inits = hmapget.size();
             FileReader fr = new FileReader(health._info_df_fp);
             BufferedReader br = new BufferedReader(fr);
             String str2 = br.readLine();        //第一行是标题, 没有用处, 读出后不处理
             while (str2 != null) {
                 str2 = br.readLine();
-                //System.out.println(str2);
+                System.out.println("str2:"+str2);
                 if(null == str2 || str2.trim().equals(""))continue;
                 String[] vs6 = new String[6]; int x = 0;
                 for(String v : str2.split(" ")){
@@ -124,30 +149,31 @@ public class Health {
                 if(pre >= Health._info_df_maxv){
                     //此处拿到有用的数据:vs6[5]-挂载点 ; vs6[4]-已用%
                     //System.out.print(vs6[5]);System.out.print("\t");System.out.print(vs6[4]);
-                    Map<String, String> hmapget = _jedis.hgetAll(rediskey);
-                    int inits = hmapget.size();
                     hmapget.put(vs6[5].replace("/", "__"), String.valueOf(pre));
 
                     Iterator<Map.Entry<String, String>> it = hmapget.entrySet().iterator();
                     while (it.hasNext()) {
                         Map.Entry<String, String> doc = it.next();
                         System.out.println(doc.getKey() + ":" + doc.getValue());
-                        if(Integer.parseInt(doc.getValue())<Health._info_df_maxv){
+                        //if(Integer.parseInt(doc.getValue())<Health._info_df_maxv){
+                        if(Double.parseDouble(doc.getValue())<Health._info_df_maxv){
                             it.remove();
                         }
                     }
 
-                    if(inits>hmapget.size()) {
-                        System.out.println("redis.hmapget: 变小, 移除旧redis, 重新写入");
-                        _jedis.del(rediskey);
-                    }
-                    _jedis.hmset(rediskey, hmapget);
-                    int ot = 60 * 60;      //过期时间: 1小时
-                    _jedis.expire(rediskey, ot);
-                    System.out.println("redis.hmapget:"+hmapget.toString());
                 }
 
             }
+            if(inits>hmapget.size()) {
+                System.out.println("redis.hmapget: 变小, 移除旧redis, 重新写入");
+                _jedis.del(rediskey);
+            }
+            //CPU温度
+            hmapget.put("temp", getCpuTemprature());
+            _jedis.hmset(rediskey, hmapget);
+            int ot = 60 * 60;      //过期时间: 1小时
+            _jedis.expire(rediskey, ot);
+            System.out.println("redis.hmapget:"+hmapget.toString());
 
             br.close();
             fr.close();
